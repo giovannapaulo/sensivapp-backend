f0a478a2f0d07426de830110872bc85ee66601ca
Giovanna Brandão <giannabrandaop@gmail.com>
2026-06-21 01:56:32 -0300

    Delete sensivapp-backend directory

--- sensivapp-backend/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AuthController = require('../controllers/AuthController');
-
-router.post('/registrar', AuthController.registrar);
-router.post('/logar', AuthController.logar);
-
-module.exports = router;
--- sensivapp-backend/src/routes/sessaoRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const SessaoController = require('../controllers/SessaoController');
-
-router.get('/:id_vinculo', SessaoController.listarPorVinculo);
-
-router.post('/', SessaoController.criarSessao);
-
-module.exports = router;
