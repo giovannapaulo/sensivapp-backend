bb5054e9b95ee6754fb2a877249cdf2488b60e7f
Giovanna Brandão <giannabrandaop@gmail.com>
2026-06-21 15:14:03 -0300

    Delete src directory

--- src/models/FavoritoArtigo.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-
-const FavoritoArtigo = sequelize.define('FavoritoArtigo', {
-    id_paciente: { type: DataTypes.INTEGER, primaryKey: true, references: { model: 'Pacientes', key: 'id_usuario' } },
-    id_artigo: { type: DataTypes.INTEGER, primaryKey: true, references: { model: 'Artigos', key: 'id_artigo' } }
-}, { tableName: 'Favoritos_Artigos', timestamps: false });
-
-module.exports = FavoritoArtigo;
--- src/models/FavoritoSom.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-
-const FavoritoSom = sequelize.define('FavoritoSom', {
-    id_paciente: { type: DataTypes.INTEGER, primaryKey: true, references: { model: 'Pacientes', key: 'id_usuario' } },
-    id_som: { type: DataTypes.INTEGER, primaryKey: true, references: { model: 'Sons_Relaxantes', key: 'id_som' } }
-}, { tableName: 'Favoritos_Sons', timestamps: false });
-
-module.exports = FavoritoSom;
